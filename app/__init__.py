a0878126f717f5d2b6a10c571d430f4bd8ce0000
hzhangg20 <hanzhang99999@gmail.com>
2022-03-08 08:27:17 -0500

    updated python files

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, redirect, request, session
+import json
+import os
+import urllib3
+import sqlite3
+import time
+
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
