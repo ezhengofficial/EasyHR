7b65abd9001fe44a2c27c73ac9ce354c6210d1e9
xVany <rayy12204@gmail.com>
2022-03-09 08:19:22 -0500

    renamed the databse calls

--- app/login.py
+++ app/login.py
@@ -4,23 +4,12 @@ import os
 import urllib3
 import sqlite3
 import time
+from db import get_db
 
-app = Flask(__name__)
-app.secret_key = os.urandom(32)
 
-db = sqlite3.connect(MAIN_DB)
-c = db.cursor()
+d = get_db()
+c = d.cursor()
 
-# table creation
-c.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    ROWID       INTEGER PRIMARY KEY,
-    username    TEXT    NOT NULL,
-    hash        TEXT    NOT NULL
-);""")
-
-db.commit()
-db.close()
 
 def isAlphaNum(string):
     """
@@ -50,8 +39,8 @@ def signup():
         print(request.form['username'] + " - " + request.form['password'])
         # Checking if required values in query exist using key values
         if 'username' in request.form and 'password' in request.form:
-            db = sqlite3.connect(MAIN_DB)
-            c = db.cursor()
+            d = get_db()
+            c = d.cursor()
             # Obtaining data from database
             c.execute("""SELECT username FROM users WHERE username = ?;""",
                       (request.form['username'],))
@@ -61,36 +50,36 @@ def signup():
                 username = (request.form['username']).encode('utf-8')
                 # Check to see if user follows formatting
                 if isAlphaNum(username.decode('utf-8')) == None:
-                    db.close()
+                    d.close()
                     return render_template("login.html", user=session.get('username'), action="/signup", name="Sign Up", error="Username can only contain alphanumeric characters.")
                 # Check to see if username is of proper length
                 if len(username) < 5 or len(username) > 15:
-                    db.close()
+                    d.close()
                     return render_template("login.html", user=session.get('username'), action="/signup", name="Sign Up", error="Usernames must be between 5 and 15 characters long")
                 password = request.form['password']
                 # Checking for illegal characters in password
                 if ' ' in list(password) or '\\' in list(password):
-                    db.close()
+                    d.close()
                     return render_template("login.html", action="/signup", name="Sign Up", error="Passwords cannot contain spaces or backslashes.")
                 password = str(password)
                 # Checking to see if password follows proper length
                 if len(password) > 7 and len(password) <= 50:
                     c.execute("""INSERT INTO users (username,hash) VALUES (?,?)""",
                               (request.form['username'], password))
-                    db.commit()
+                    d.commit()
                     c.execute(
                         """SELECT username FROM users WHERE username = ?;""", (request.form['username'],))
                     exists = c.fetchone()
-                    db.close()
+                    d.close()
                     if (exists != None):
                         return render_template("login.html", action="/login", name="Login", success="Signed up successfully!")
                     else:
                         return render_template("login.html", action="/signup", name="Sign Up", error="Some error occurred. Please try signing up again.")
                 else:
-                    db.close()
+                    d.close()
                     return render_template("login.html", action="/signup", name="Sign Up", error="Password must be between 8 and 50 characters long")
             else:
-                db.close()
+                d.close()
                 return render_template("login.html", action="/signup", name="Sign Up", error="Username already exists")
         else:
             return render_template("login.html", action="/signup", name="Sign Up", error="Some error occurred. Please try signing up again.")
@@ -108,12 +97,12 @@ def login():
         if 'username' in session:
             return render_template("index.html", user=session.get('username'), message="Already logged in!")
         if 'username' in request.form and 'password' in request.form:
-            db = sqlite3.connect(MAIN_DB)
-            c = db.cursor()
+            d = get_db()
+            c = d.cursor()
             c.execute("""SELECT hash FROM users WHERE username = ?;""",
                       (request.form['username'],))
             hashed = c.fetchone()  # [0]
-            db.close()
+            d.close()
             if (hashed == None):
                 return render_template("login.html", name="Login", action="/login", error="Invalid username or password")
             else:
