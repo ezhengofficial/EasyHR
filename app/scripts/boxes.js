c5092d37970928313d3edef67456e86c89ca428b
lin-zhaoyu <zlin20@stuy.edu>
2022-03-15 08:37:19 -0400

    grid change

--- app/scripts/boxes.js
+++ app/scripts/boxes.js
@@ -1,12 +1,22 @@
+
+console.log("hello");
 function createGrid(x) {
     for (var rows = 0; rows < x; rows++) {
         for (var columns = 0; columns < x; columns++) {
             $("#grid-container").append("<div class='grid'></div>");
         };
     };
-    $(".grid").width(960/x);
-    $(".grid").height(960/x);
+    $(".grid").width(500/x);
+    $(".grid").height(500/x);
 };
+function refreshGrid(x){
+  $(".grid").remove();
+  createGrid(x)
+}
 $(document).ready(function() {
     createGrid(6);
+    $(".newGrid").click(function(){
+      refreshGrid(6);
+      console.log("newGrid");
+    });
   });
