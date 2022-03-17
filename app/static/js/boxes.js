09dbbc4539fbfe5b4debeb7e0145b0170d06e0af
lin-zhaoyu <zlin20@stuy.edu>
2022-03-17 08:35:44 -0400

    keyboard to front page

--- app/static/js/boxes.js
+++ app/static/js/boxes.js
@@ -1,14 +1,23 @@
 
 console.log("hello");
 function createGrid(x) {
+  let board = document.getElementById("grid-container");
+
     for (var rows = 0; rows < x; rows++) {
-        for (var columns = 0; columns < x; columns++) {
-            $("#grid-container").append("<div class='grid'></div>");
+      let row = document.createElement("div");
+      row.className = "row";
+        for (var columns = 0; columns < 5; columns++) {
+          let box = document.createElement("div");
+          box.className = "grid";
+          row.appendChild(box);
         };
+        board.appendChild(row)
     };
-    $(".grid").width(500/x);
-    $(".grid").height(500/x);
+    // $(".grid").width(500/x);
+    // $(".grid").height(500/x);
 };
+
+
 function refreshGrid(x){
   $(".grid").remove();
   createGrid(x)
