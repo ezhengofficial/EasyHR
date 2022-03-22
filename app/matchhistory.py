c320fc40e466d2c298eb3286c8653ff41efad7a2
xVany <rayy12204@gmail.com>
2022-03-22 08:41:27 -0400

    update match history

--- app/matchhistory.py
+++ app/matchhistory.py
@@ -9,38 +9,47 @@ from db import get_db
 bp = Blueprint('matchhistory', __name__)
 def record():
     userpath = "userfiles/%s.txt" % session['username']
-    #session['guesses'] is a list of words in the form of :c1r2a0n1e2; 0 -> gray, 1 -> yellow, 2 -> green
+    #session['guesses'] is a list of words in the form of :c1r2a0n1e2; 0 -> green 1 - yellow 2 gray
     with open (userpath, "r+") as userfile:
         len = session['game']['guesses'].length()
-        userfile.write(date.today().strftime("%m/%d/%y"), "\n")
+        userfile.write(date.today().strftime("%m/%d/%y"), len, "\n")
         for i in range(len):
             userfile.write(session['game']['guesses'][i], "|")
         for i in range(len):
-            userfile.write(session['game']['colors'][i], ",")
+            for i in range(5):
+                userfile.write(session['game']['colors'][i], ",")
         userfile.write("\n")
 
+#Guesses, Date, GuessCount, colors
 
 @bp.route("/history", methods=['GET', 'POST'])
 def getHistory():
-    history = []
-    chistory = []
-    userpath = "userfiles/%s.txt" % session['username']
-    with open (userpath, "r+") as userfile:
-        lines = userfile.readlines()
-        for line in lines:
-            if "/" in line:
-                date_history = []
-                c = []
-                date_history.append(line)
-            if "|" in line:
-                words = line.split('|')
-                for word in words:
-                    date_history.append(word)
-            if "," in line:
-                colors = line.split(',')
-                for color in colors:
-                    c.append(colors)
-            if line == "\n":
-                chistory.append(c)
-                history.append(date_history)
-    return render_template("history.html", matches=history, guesscolors=chistory)
+    if request.method == "GET":
+        history = []
+        chistory = []
+        guesscount = []
+        date_history = []
+        userpath = "userfiles/%s.txt" % session['username']
+        with open (userpath, "r+") as userfile:
+            lines = userfile.readlines()
+            for line in lines:
+                if "/" in line:
+                    c = []
+                    date_history.append(line.rstrip("\n"))
+                if "|" in line:
+                    words = line.split('|')
+                    print(words)
+                    for word in words:
+                        date_history.append(word.rstrip("\n"))
+                if "," in line:
+                    colors = line.rstrip("\n").split(',')
+                    for color in colors:
+                        c.append(color)
+                if line.isdigit():
+                    guesscount.append(line.rstrip("\n"))
+                if line == "\n":
+                    print('yes')
+                    chistory.append(c)
+                    history.append(date_history)
+        print(guesscount)
+        return render_template("history.html", dates=date_history, matches=history, guesscolors=chistory, count=guesscount)
