b4ba257915eedc0189f9a3ccb41a09e62e06b12b
hzhangg20 <hanzhang99999@gmail.com>
2022-03-23 23:33:41 -0400

    edit

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,10 @@ from datetime import date
 app = Flask(__name__)
 
 app.secret_key = os.urandom(32),
+DATABASE = os.path.join(os.path.dirname(__file__), "database.db")
+db = sqlite3.connect(DATABASE, check_same_thread=False, timeout=10)
+c = db.cursor()
+
 
 init_db()
 
@@ -28,10 +32,10 @@ def home():
         if c.fetchone() != date.today():
             c.execute("""UPDATE users SET lastplayed = ? WHERE username = ?;""",
                   (date.today(), session.get('username')))
-            
-
+        c.close()
         return render_template("home.html")
     else:
+        c.close()
         return redirect("/login")
 
 @app.route("/leaderboard", methods=['GET', 'POST'])
